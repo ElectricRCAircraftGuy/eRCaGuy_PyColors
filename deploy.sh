7995cffc2e398fbcdb16803b96f1c787ae19d4c5
Gabriel Staples <ercaguy@gmail.com>
2026-01-13 00:02:23 -0700

    deploy.sh: improve error print

--- deploy.sh
+++ deploy.sh
@@ -40,8 +40,8 @@ if [ $? -eq 0 ]; then
     echo "Tagged and pushed v${VERSION}"
 else
     echo ""
-    echo "ERROR: Upload failed. Not tagging in git. You probably need to increment the version"
-    echo "number in 'eRCaGuy_PyColors/__init__.py' to trigger a new upload."
+    echo "ERROR: Upload to PyPI failed. Not tagging in git. You probably need to increment the" \
+         "version number in 'eRCaGuy_PyColors/__init__.py' to trigger a new upload."
     exit 1
 fi
 
